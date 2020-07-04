c46cc4b32b8e23cfb05dbdfcf33cc1b014326f7c
Cody C <codyicastro@gmail.com>
2020-07-04 17:27:47 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,13 @@
-def count_strings(array)
-  array.count do |element|
-    array[element].include?(String)
+[1, 2, 3,].count do |element|
+  # code in here runs every time count enumerates over an element
+  # that element is available as the name we define inside the pipes above
 end
-end
+
+# def count_strings(array)
+#   array.count do |element|
+#     array[element].include?(String)
+# end
+# end
 
 
 
